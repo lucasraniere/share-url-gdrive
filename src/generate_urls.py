7ee28b4a114824c7c02b9df0cfcddaeacc704466
Lucas Raniére <lucas.raniere94@gmail.com>
2024-06-25 17:04:52 -0400

    added try finally, and now the script checks if the file already existis

--- src/generate_urls.py
+++ src/generate_urls.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
@@ -7,7 +8,8 @@ g_auth.LocalWebserverAuth()
 
 drive = GoogleDrive(g_auth)
 
-FOLDER_ID = ''
+FILE_NAME = 'arquivos_links.csv'
+FOLDER_ID = '1X7Z79R7cRhS-USq1WmWe-nbFBEhbXNOY'
 query = "'{}' in parents and trashed=false".format(FOLDER_ID)
 PERMISISONS = {
     'type': 'anyone',
@@ -16,15 +18,21 @@ PERMISISONS = {
 }
 
 def main():
+    files = []
     record = []
     file_list = drive.ListFile({'q': query}).GetList()
     file_amount = len(file_list)
-    for idx, file in enumerate(file_list):
-        print('Processando arquivo {} de {}...'.format(idx+1, file_amount))
-        file.InsertPermission(PERMISISONS)
-        record.append({'title': file['title'], 'link' : file['alternateLink']})
-    df = pd.DataFrame.from_records(record)
-    df.to_csv('file_list.csv', index=False)
+    if FILE_NAME in os.listdir():
+        files = pd.read_csv(FILE_NAME)['title'].values
+    try:
+        for idx, file in enumerate(file_list):
+            if file['title'] not in files:
+                print('Processando arquivo {} de {}...'.format(idx+1, file_amount))
+                file.InsertPermission(PERMISISONS)
+                record.append({'title': file['title'], 'link' : file['alternateLink']})
+    finally:
+        df = pd.DataFrame.from_records(record)
+        df.to_csv(FILE_NAME, index=False)
 
 
 if __name__ == '__main__':
