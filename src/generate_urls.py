165ba92d165d0ed46b97ee185079c7c6f000bbb2
Lucas Raniére <lucas.raniere94@gmail.com>
2024-06-25 14:22:12 -0400

    now im changing the permission so anyone with the url can access

--- src/generate_urls.py
+++ src/generate_urls.py
@@ -9,14 +9,18 @@ drive = GoogleDrive(g_auth)
 
 FOLDER_ID = ''
 SHARE_URL = 'https://drive.google.com/file/d/{}/view?usp=sharing'
-
-query = "'{}' in parents and trashed=false".format(FOLDER_ID)
+PERMISISONS = {
+    'type': 'anyone',
+    'value': 'anyone',
+    'role': 'reader'
+}
 
 def main():
     record = []
     file_list = drive.ListFile({'q': query}).GetList()
     for file in file_list:
-        record.append({'title': file['title'], 'link' : SHARE_URL.format(file['id'])})
+        file.InsertPermission(PERMISISONS)
+        record.append({'title': file['title'], 'link' : file['alternateLink']})
     df = pd.DataFrame.from_records(record)
     df.to_csv('file_list.csv', index=False)
 
